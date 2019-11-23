d62f261b5f78dbadc49fb404763c1823c0716a14
PIYUSH KUMAR <pikumar@quotient.com>
2019-11-23 15:21:46 +0530

    Creating Producer Demo

---
+++ src/main/java/Kafka_Basics/ProducerDemo.java
@@ -0,0 +1,39 @@
+package Kafka_Basics;
+/**
+ * Created By : Piyush Kumar
+ */
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static void main(String[] args){
+        String bootstrapServer = "127.0.0.1:9092";
+
+        //Producer Properties
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        //Creating a Producer
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // Creating ProducerRecord
+
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic","Hi_New_to_Kafka_time");
+
+        // Send record
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
+    }
+}
